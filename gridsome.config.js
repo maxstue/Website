599cfdb722d14923a7106f9ac9403901d8d94c69
Max <maximilian2304@t-online.de>
2019-05-01 17:56:16 +0200

    new initial

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,5 +6,35 @@
 
 module.exports = {
   siteName: 'Gridsome',
-  plugins: []
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        // ...global plugins
+      ]
+    }
+  },
+  plugins: [
+    {
+      use: 'gridsome-plugin-netlify-cms',
+      options: {
+        // modulePath: `src/cms/index.js`,
+        configPath: `static/admin/config.yml`,
+        // htmlPath: `src/admin/index.html`,
+        publicPath: `/admin`,
+        htmlTitle: `My CMS`
+      }
+    } ,
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'blog/**/*.md',
+        typeName: 'Post',
+        route: '/blog/:slug',
+
+      }
+    },
+  ]
 }
