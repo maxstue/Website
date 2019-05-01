cb8e6eda12ed5c29c81da1798f5c5e6e844b59dd
Max <maximilian2304@t-online.de>
2019-05-01 19:06:25 +0200

    added 404 custom page, and testet neetlify cms on about me page

---
+++ src/pages/404.vue
@@ -0,0 +1,30 @@
+<template>
+  <Layout>
+    
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+    
+    <h1>I am Sorry,but something went wrong!</h1>
+   
+    <p>
+        Error Code 404
+    </p>
+
+    
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,9 +1,34 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <div id="one" class="main style1">
+        <div class="container">
+            <div class="row gtr-150">
+                <div class="col-6 col-12-medium">
+                    <header class="major">
+                        <h2>{{ $static.post.title }}</h2>
+                    </header>
+                    <div v-html="$static.post.content"/>
+                </div>
+                <div class="col-6 col-12-medium imp-medium">
+                    <span class="image fit"><img src="../assets/images/pic01.jpg" alt="" /></span>
+                </div>
+            </div>
+        </div>
+    </div>
   </Layout>
 </template>
+
+<static-query>
+query Post {
+  post: post (path: "/blog/this-is-a-test-about-me-page") {
+    title
+    content
+    path
+    date
+    slug
+  }
+}
+</static-query>
 
 <script>
 export default {
