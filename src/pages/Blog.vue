1fa0f4eb1c5e0c3abcdca07dc1031ea6efb1b37c
Max <maximilian2304@t-online.de>
2019-05-17 16:11:02 +0200

    little blog content styling

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -4,26 +4,36 @@
     <div v-for="edge in $page.posts.edges" :key="edge.node.id">
       <div id="one" class="main style1">
         <div class="container">
-          <div class="row gtr-150 card">
-            <div class="col-6 col-12-medium">
-              <header class="major">
-                <g-link :to="edge.node.path">
+          <g-link :to="edge.node.path">
+            <div class="row gtr-150 card">
+              <div class="col-6 col-12-medium">
+                <header class="major">
                   <h2 id="blogTitle">{{ edge.node.title }}</h2>
-                </g-link>
-                <div class="seperator_line"></div>
-              </header>
-              <div v-html="edge.node.content"/>
-            </div>
+                  <div class="date">{{edge.node.date.split("T")[0]}}</div>
+                  <!-- <div class="seperator_line"></div> -->
+                </header>
+                <div
+                  class="content"
+                  v-if="edge.node.content.length < 200"
+                  v-html="edge.node.content + ' ...' "
+                />
+                <div
+                  class="content"
+                  v-if="edge.node.content.length >= 300"
+                  v-html="edge.node.content.substring(0,300) + ' ...' "
+                />
+              </div>
 
-            <div class="col-6 col-12-medium imp-medium">
+              <!-- <div class="col-6 col-12-medium imp-medium">
               <span class="image fit">
                 <g-link :to="edge.node.path">
                   <img src="../assets/images/pic01.jpg" alt>
                 </g-link>
               </span>
               <span>{{edge.node.date}}</span>
+              </div>-->
             </div>
-          </div>
+          </g-link>
         </div>
       </div>
     </div>
@@ -69,21 +79,11 @@ query Post {
     font-weight: bold;
   }
 
-  hr {
-    border: 0;
-    border-bottom: solid 1px rgba(144, 144, 144, 0.5);
-    margin: 0em 7% 0em 7%; /*top right bottom left*/
-  }
-
   #blogTitle {
-    color: black;
-    font-weight: bolder;
+    font-weight: 400;
     text-decoration: none;
     text-decoration-color: black;
-  }
-
-  #blogTitle:visited:focus{
-    text-decoration: none;
+    color: var(--text-greyish);
   }
 
   .seperator_line {
@@ -94,5 +94,27 @@ query Post {
     margin-top: 1.5em;
     width: 10em;
     margin: 0em 1em 0em 0em;
+  }
+
+  .content {
+    text-decoration: none;
+    color: var(--text-greyish);
+    font-size: 15px;
+  }
+
+  .date {
+    margin: 5px 0 0 0;
+    text-decoration: none;
+    color: black;
+    font-size: 15px;
+    font-weight: 700;
+  }
+
+  .row > .col-6 {
+    width: 100%;
+  }
+
+  .card {
+    padding: 0 5em 0 2em;
   }
 </style>
