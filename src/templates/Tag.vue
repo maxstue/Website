e0a396b8d7077fc1b85a58c9232243f91b621064
Max <maximilian2304@t-online.de>
2019-06-23 11:45:01 +0200

    save state

--- src/templates/Tag.vue
+++ src/templates/Tag.vue
@@ -3,17 +3,21 @@
     <div class="container-inner mx-auto my-16">
       <h2 class="text-4xl font-bold mb-8 border-b">Tag: {{ $page.tag.title }}</h2>
 
-      <div v-for="post in $page.tag.belongsTo.edges" :key="post.node.id" class="post border-gray-400 border-b mb-12">
-        <h2 class="text-3xl font-bold"><g-link :to="post.node.path" class="text-copy-primary">{{ post.node.title }}</g-link></h2>
+      <div
+        v-for="post in $page.tag.belongsTo.edges"
+        :key="post.node.id"
+        class="post border-gray-400 border-b mb-12"
+      >
+        <h2 class="text-3xl font-bold">
+          <g-link :to="post.node.path" class="text-copy-primary">{{ post.node.title }}</g-link>
+        </h2>
         <div class="text-copy-secondary mb-4">
           <span>{{ post.node.date }}</span>
-          <span> &middot; </span>
+          <span>&middot;</span>
           <span>{{ post.node.timeToRead }} min read</span>
         </div>
 
-        <div class="text-lg mb-4">
-          {{ post.node.summary }}
-        </div>
+        <div class="text-lg mb-4">{{ post.node.summary }}</div>
 
         <div class="mb-8">
           <g-link :to="post.node.path" class="font-bold uppercase">Read More</g-link>
@@ -26,7 +30,6 @@
         :totalPages="$page.tag.belongsTo.pageInfo.totalPages"
         :currentPage="$page.tag.belongsTo.pageInfo.currentPage"
       />
-
     </div>
   </Layout>
 </template>
@@ -61,16 +64,16 @@ query Tag ($id: String!, $page: Int) {
 </page-query>
 
 <script>
-import PaginationPosts from '../components/PaginationPosts'
+import PaginationPosts from "../components/PaginationPosts";
 
 export default {
   metaInfo() {
     return {
-      title: 'Tag: ' + this.$page.tag.title
-    }
+      title: "Tag: " + this.$page.tag.title
+    };
   },
   components: {
     PaginationPosts
   }
-}
+};
 </script>
